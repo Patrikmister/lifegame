39ab4ffacb0f683b728470bd61baa41eabe2deba
Egor Patrikeev <patrikmister@yandex.ru>
2018-05-25 17:06:36 +0300

    Prototypes added

---
+++ lifegame/lifegame/prototypes.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/*******************************PROTOTYPES OF ALL USED FUNCTIONS*************************************/
+
+struct point {}; //Contains the current condition of the point
+void playground_initialize(point); //Initializes the playground
+int amount_of_alive(point); //Counts the amount of alive points
